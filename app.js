131fc2c27c4b9de81a0e0331371a2e7e2ffcd633
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2019-05-06 19:59:40 +0530

    adding nodemon and removing path from app.js

--- app.js
+++ app.js
@@ -39,27 +39,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-var childProcess = require('child_process');
-var githubUsername = 'gayansandaruwan'
-
-
-app.post("/webhooks/github", function (req, res) {
-    var sender = req.body.sender;
-    var branch = req.body.ref;
-
-    if(branch.indexOf('master') > -1 && sender.login === githubUsername){
-        deploy(res);
-    }
-})
-
-function deploy(res){
-    childProcess.exec('cd /home/gayankavirathne/PDRMax/node-dep-auto-webhook/ && ./clone_or_update.sh', function(err, stdout, stderr){
-        if (err) {
-         console.error(err);
-         return res.send(500);
-        }
-        res.send(200);
-      });
-}
-
 module.exports = app;
